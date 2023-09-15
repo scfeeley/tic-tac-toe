a038278721475d9c7fd879f8eec2106da864a4a8
Shannon Feeley <scfeeley@wpi.edu>
2023-09-15 15:23:37 -0600

    added button functionality

--- public/tic-tac-toe.js
+++ public/tic-tac-toe.js
@@ -8,7 +8,6 @@ window.onload = (event => {
     board.addEventListener("click", handleBoardClick);
 
     let newBtn = document.querySelector("#new");
-    console.log(newBtn)
     newBtn.addEventListener("click", handleNewClick);
 
     let giveUpBtn = document.querySelector("#give-up")
@@ -55,10 +54,16 @@ window.onload = (event => {
         ele.classList.add("right");
     });
 
+    //Gray out new game button
+    if(!winner){
+        newBtn.style.backgroundColor = "gray";
+    }
+
 
     function handleBoardClick(event) {
         if(!winner){
             let square = event.target;
+            console.log(square)
             if (square.children.length === 0) {
                 //place token
                 let next = nextToken["next"];
@@ -73,11 +78,11 @@ window.onload = (event => {
                 console.log("filled")
                 //check for winner
                 let win = checkWinner();
-                console.log("winner checked")
-                console.log(win);
                 if (win) {
                     title.innerHTML = "Winner: " + win.toUpperCase();
                     winner = win;
+                    newBtn.style.backgroundColor = "white";
+                    giveUpBtn.style.backgroundColor = "gray";
                 } else if (filled === 9) {
                     //check for tie
                     title.innerText = "Tie Game"
@@ -89,20 +94,30 @@ window.onload = (event => {
     }
 
     function handleNewClick(event){
-        console.log("clicked");
+        if(!winner){
+            console.log("cant click this");
+        }else{
+            resetGameBoard();
+        }
     }
 
     function handleGiveUpClick(event){
-        console.log("gave up")
+        if(winner){
+            console.log("can't click this");
+        }else{
+           resetGameBoard();
+        }
     }
 
 
-        
+    //Check all rows, columns, diagonals for a winner
     function checkWinner(){
         return checkNodeList(r1) || checkNodeList(r2) || checkNodeList(r3)
         || checkNodeList(c1) || checkNodeList(c2) || checkNodeList(c3) ||
         checkNodeList(d1) || checkNodeList(d2);
     } 
+
+    //check 3 nodes to see if they match
     function checkNodeList(r) {
         let rows = []; 
         r.forEach(ele => {
@@ -114,6 +129,8 @@ window.onload = (event => {
         });
         return checkMatch(rows);
     }
+
+    //check to see if 3 strings match
     function checkMatch(l1){
         for(let i = 0; i < l1.length - 1; i++){
             if(l1[i] !== l1[i+1]){
@@ -121,5 +138,20 @@ window.onload = (event => {
             }
         }
         return l1[0];
-    };
-})  
+    }
+    
+    //Reset Game Board 
+    function resetGameBoard() {
+        title.innerText = "Tic-Tac-Toe";
+        let tokensPlayed = document.querySelectorAll(".squares>div");
+        tokensPlayed.forEach(tok => {
+            tok.remove(); 
+        });
+        winner = undefined;
+        nextToken["next"] = "x";
+        filled = 0;
+        giveUpBtn.style.backgroundColor = "white";
+        newBtn.style.backgroundColor = "gray";
+
+    }
+}) 
